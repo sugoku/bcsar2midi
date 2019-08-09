521d274aff871b2cb3a8c35a9de1b19a43d6a611
MDHEXT <42657178+MDHEXT@users.noreply.github.com>
2019-08-08 22:19:01 -0600

    Update bcsar cseq2midi protocall.bat

--- bcsar cseq2midi protocall.bat
+++ bcsar cseq2midi protocall.bat
@@ -6,6 +6,6 @@ PAUSE
 FOR %%C IN (*.bcseq) DO tools\cseq2midi.exe %%C
 FOR %%C IN (*.cseq) DO tools\cseq2midi.exe %%C
 DEL cseq2midi.log.txt
-cd C:\Users\sgonzalez08\Desktop\bcsar2midi\tools
+cd C:\Users\Your username here\Desktop\bcsar2midi\tools
 vgconvert.bat
 
