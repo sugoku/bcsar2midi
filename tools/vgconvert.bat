ef7606555bfd6c8855ccc56c745818489a06f139
MDHEXT <42657178+MDHEXT@users.noreply.github.com>
2019-08-08 22:20:19 -0600

    Update vgconvert.bat

--- tools/vgconvert.bat
+++ tools/vgconvert.bat
@@ -1,10 +1,10 @@
-VGAudioCli -b -i C:\Users\sgonzalez08\Desktop\bcsar2midi\bcwav -o C:\Users\sgonzalez08\Desktop\bcsar2midi --out-format wav
-cd C:\Users\sgonzalez08\Desktop\bcsar2midi
+VGAudioCli -b -i C:\Users\your username here\Desktop\bcsar2midi\bcwav -o C:\Users\your username here\Desktop\bcsar2midi --out-format wav
+cd C:\Users\your username here\Desktop\bcsar2midi
 FOR %%C IN (*.txt) DO tools\sf2comp c "%%C"
 DEL *.txt
 DEL *.bcseq
 DEL *.wav
-cd C:\Users\sgonzalez08\Desktop\bcsar2midi\bcwav
+cd C:\Users\your username here\Desktop\bcsar2midi\bcwav
 DEL *.bcwav
 echo The extraction process is complete! Thanks for using this tool!
 PAUSE
