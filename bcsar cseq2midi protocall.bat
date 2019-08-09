00077e820e8e94c9e58ce09b01887ab8db91d5ac
MDHEXT <42657178+MDHEXT@users.noreply.github.com>
2019-08-08 21:20:35 -0600

    Add files via upload

---
+++ bcsar cseq2midi protocall.bat
@@ -0,0 +1,11 @@
+@Echo Off
+FOR %%C IN (*.bcsar) DO tools\Wii3DSUSoundTool.exe %%C
+FOR %%C IN (*.csar) DO tools\Wii3DSUSoundTool.exe %%C
+echo put all bcseq and txt archives in root folder (the one with the bcsar files in it) and put the bcwav files in the bcwav folder
+PAUSE
+FOR %%C IN (*.bcseq) DO tools\cseq2midi.exe %%C
+FOR %%C IN (*.cseq) DO tools\cseq2midi.exe %%C
+DEL cseq2midi.log.txt
+cd C:\Users\sgonzalez08\Desktop\bcsar2midi\tools
+vgconvert.bat
+
