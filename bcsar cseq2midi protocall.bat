9b3ecc37edbfd043420cd8028fb213de0e7f5404
MDHEXT <42657178+MDHEXT@users.noreply.github.com>
2019-08-18 15:38:54 -0600

    Update bcsar cseq2midi protocall.bat
    Add instructions to end, make one bat file instead of relying on vgconvert.bat

--- bcsar cseq2midi protocall.bat
+++ bcsar cseq2midi protocall.bat
@@ -7,5 +7,16 @@ FOR %%C IN (*.bcseq) DO tools\cseq2midi.exe %%C
 FOR %%C IN (*.cseq) DO tools\cseq2midi.exe %%C
 DEL cseq2midi.log.txt
 cd C:\Users\Your username here\Desktop\bcsar2midi\tools
-vgconvert.bat
+VGAudioCli -b -i C:\Users\Your username here\Desktop\bcsar2midi\bcwav -o C:\Users\Your username here\Desktop\bcsar2midi --out-format wav
+cd C:\Users\Your username here\Desktop\bcsar2midi
+FOR %%C IN (*.txt) DO tools\sf2comp c "%%C"
+DEL *.txt
+DEL *.bcseq
+DEL *.wav
+cd C:\Users\Your username here\Desktop\bcsar2midi\bcwav
+DEL *.bcwav
+echo The extraction process is complete! Make sure to edit the samples in the soundfont as they are not looped. Looping the samples will result in a higher quality rip. Thanks for using this tool!
+PAUSE
+EXIT
+
 
